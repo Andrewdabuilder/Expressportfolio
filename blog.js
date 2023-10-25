d216b496748690db424c7750f06f5bb637c89a71
andrewdabuilder <andrew@filterhealth.co>
2023-10-25 11:22:42 +0100

    Update to backend, not functional yet

--- blog.js
+++ blog.js
@@ -14,21 +14,93 @@ app.use(express.urlencoded({ extended: false }))
 
 // load own modules
 const createUsers = require('./modules/createUsers')
+const removeUsers = require('./modules/removeUsers')
 const createBlogs = require('./modules/createBlogs')
+const removeBlogs = require('./modules/removeBlogs')
 
 // define the routes
 app.get('/', (req, res) => {
     res.send('Hello, this is the Blog Home Page.')
   })
 
+app.get('/blog/:year?/:month?/:day?', (req, res) => {
+
+  url = req.url
+  year = req.params.year
+  month = req.params.month
+  day = req.params.day
+
+  var allBlogs = queryBlogs.queryAllBlogs()
+
+    if (url == '/blog') {
+      if (allBlogs.length == 0) {
+        res.send('no post found')
+      } else {
+        res.send(allBlogs)
+      }
+    }
+
+    else if (url == '/blog/'+year) {
+
+      const blogsYear = queryBlogs.queryBlogsYear(allBlogs)
+
+      if(blogsYear.length == 0) {
+        res.send('no post found for this year')
+      } else {
+        res.send(blogsYear)
+      }
+
+    }
+
+    else if (url == '/blog/'+year+'/'+month) {
+
+      const blogsYearMonth = queryBlogs.queryBlogsYearMonth(allBlogs)
+
+      if(blogsYearMonth.length == 0) {
+        res.send('no post found for this year and month')
+      } else {
+        res.send(blogsYearMonth)
+      }
+
+    }
+
+    else if (url == '/blog/'+year+'/'+month+'/'+day) {
+
+      const blogsYearMonthDay = queryBlogs.queryBlogsYearMonthDay(allBlogs)
+
+      if(blogsYearMonthDay.length == 0) {
+        res.send('no post found for this year and month and day')
+      } else {
+        res.send(blogsYearMonthDay)
+      }
+
+    }
+
+    else {
+      res.status(404).send('Not Found')
+    }
+
+})
+
 app.post('/createusers', (req, res) => {
     createUsers(req, res)
     res.send('User has been added successfully.')
   })
+
+app.post('/removeusers', (req, res) => {
+    removeUsers(req, res)
+    res.send('User has been successfully removed.')
+  })
+
   
-  app.post('/createblogs', (req, res) => {
+app.post('/createblogs', (req, res) => {
     createBlogs(req, res)
     res.send('Blog has been added successfully.')
+  })
+  
+app.post('/removeblogs', (req, res) => {
+    removeBlogs(req, res)
+    res.send('Blog has been successfully removed.')
   })
 
 
